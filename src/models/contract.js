c3b0c90909d3e94f107717c1bc5bc549c914b8a9
Bảo Nguyễn <baonguyenhuynhquocbl03@gmail.com>
2025-11-26 02:57:03 +0700

    fix(forgot-password): not send OTP

--- src/models/contract.js
+++ src/models/contract.js
@@ -1,40 +1,40 @@
 const mongoose = require("mongoose");
 
 const contractSchema = new mongoose.Schema(
-    {
-        staffId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Staff",
-            required: true,
-            unique: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-        fromDate: {
-            type: Date,
-            required: true,
-        },
-        toDate: {
-            type: Date,
-            required: true,
-        },
-        status: {
-            type: String,
-            enum: ["active", "expired"],
-            default: "active",
-        },
-        createdBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Staff",
-            required: true,
-        },
+  {
+    staffId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Staff",
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true,
-        collection: "tblcontracts",
-    }
+    content: {
+      type: String,
+      required: true,
+    },
+    fromDate: {
+      type: Date,
+      required: true,
+    },
+    toDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "expired"],
+      default: "active",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Staff",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "tblcontracts",
+  },
 );
 
 contractSchema.index({ status: 1 });
