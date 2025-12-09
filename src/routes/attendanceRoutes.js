0f9e630ae77dd4d2a6b8d29ae3e1b529528023e7
QuocThang1 <160698477+QuocThang1@users.noreply.github.com>
2025-12-09 21:20:22 +0700

    fix: sửa phân quyền để lấy dữ liệu cho dashboard của admin

--- src/routes/attendanceRoutes.js
+++ src/routes/attendanceRoutes.js
@@ -15,7 +15,7 @@ const routerAPI = express.Router();
 routerAPI.post("/check-in", checkRole("staff"), checkIn);
 routerAPI.post("/check-out", checkRole("staff"), checkOut);
 routerAPI.get("/today", checkRole("staff"), getTodayAttendance);
-routerAPI.get("/", checkRole("manager"), getAllAttendances);
+routerAPI.get("/", checkRole("manager", "admin"), getAllAttendances);
 routerAPI.get("/my-attendances", checkRole("staff"), getMyAttendances);
 routerAPI.get(
   "/department/:departmentId",
